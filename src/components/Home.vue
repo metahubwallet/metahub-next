ec398ed207543fdb80f4e07805890ea7228eb471
mht <dev@metahub.cash>
2023-03-23 17:39:11 +0800

    feat: add router

---
+++ src/components/Home.vue
@@ -0,0 +1,33 @@
+<template>
+    <img :src="imageUrl" height="45" alt="" />
+    <!-- header -->
+    <h1>
+        Home
+    </h1>
+    <button type="button" @click="increment">Clicks: {{ count }}</button>
+    <p>
+        <router-link to="/">Go to Home</router-link><br />
+        <router-link to="/test">Go to Test</router-link>
+    </p>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import logo from '@/assets/logo.svg';
+
+const imageUrl = new URL(logo, import.meta.url).href;
+
+const count = ref(0);
+
+const increment = () => (count.value += 1);
+</script>
+
+<style>
+h1 {
+    margin: 5px;
+}
+
+button {
+    margin-top: 10px;
+}
+</style>
--- src/entries/popup/main.ts
+++ src/entries/popup/main.ts
@@ -1,5 +1,10 @@
 import { createApp } from 'vue';
+import { router } from '../../router';
 import App from './App.vue';
 import 'element-plus/dist/index.css';
 
-createApp(App).mount('#app');
+// todo: 弃用element ui，改用 naive ui
+
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
