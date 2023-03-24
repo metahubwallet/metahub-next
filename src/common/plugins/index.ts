e14fdc875c6ea57718780a5e44b5a3dac5dc950f
16 <soup16-work@outlook.com>
2023-03-24 10:37:41 +0800

    feat: tailwindcss

--- src/common/plugins/index.ts
+++ src/common/plugins/index.ts
@@ -1,6 +1,8 @@
 import { App } from 'vue';
 import { setupPinia } from './pinia';
+import { setupTailwindcss } from './tailwindcss';
 
 export const setupPlugins = (app: App) => {
+    setupTailwindcss();
     setupPinia(app);
 };
--- src/entries/popup/App.vue
+++ src/entries/popup/App.vue
@@ -1,6 +1,3 @@
-<script setup lang="ts"></script>
-
 <template>
-    <n-button type="warning">Warning</n-button>
-    <Main>Popup</Main>
+    <button class="bg-slate-600">123</button>
 </template>
