27d5097840dd0e078a581530684c1950e86406b9
16 <soup16-work@outlook.com>
2023-06-07 10:30:30 +0800

    fix: crypto doesn't work

--- config/declare/component.d.ts
+++ config/declare/component.d.ts
@@ -9,8 +9,11 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
+    Global: typeof import('./../../src/component/Global.vue')['default']
     NButton: typeof import('naive-ui')['NButton']
     NConfigProvider: typeof import('naive-ui')['NConfigProvider']
+    NInput: typeof import('naive-ui')['NInput']
+    PasswordUnlock: typeof import('./../../src/entries/popup/wallet/component/PasswordUnlock.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
--- config/vite/plugin/component.ts
+++ config/vite/plugin/component.ts
@@ -4,7 +4,7 @@ import { NaiveUiResolver } from 'unplugin-vue-components/resolvers';
 export const setupComponent = () => {
     return Components({
         // 需要自动注册的组件目录
-        dirs: ['src/components'],
+        dirs: ['src/**/component'],
         resolvers: [NaiveUiResolver()],
         // 声明文件位置
         dts: 'config/declare/component.d.ts',
