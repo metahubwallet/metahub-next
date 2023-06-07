8aa0d60850ad2658638a68765f4a08a2f9320951
16 <soup16-work@outlook.com>
2023-06-07 17:43:55 +0800

    config: IconPark

--- config/vite/plugin/component.ts
+++ config/vite/plugin/component.ts
@@ -5,7 +5,14 @@ export const setupComponent = () => {
     return Components({
         // 需要自动注册的组件目录
         dirs: ['src/**/component'],
-        resolvers: [NaiveUiResolver()],
+        resolvers: [
+            NaiveUiResolver(),
+            (componentName) => {
+                if (componentName.startsWith('Icon')) {
+                    return { name: componentName.slice(4), from: '@icon-park/vue-next' };
+                }
+            },
+        ],
         // 声明文件位置
         dts: 'config/declare/component.d.ts',
     });
