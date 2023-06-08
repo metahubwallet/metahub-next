bc911700de934602a3a6e28a00888005b16e6fe9
16 <soup16-work@outlook.com>
2023-06-08 22:53:03 +0800

    config: animate.css

--- src/entries/popup/main.ts
+++ src/entries/popup/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue';
 import { router } from '../../router';
 import App from './App.vue';
 import '@/asset/css/base.scss';
+import 'animate.css';
 
 import { setupPlugins } from '@/common/plugin';
 
