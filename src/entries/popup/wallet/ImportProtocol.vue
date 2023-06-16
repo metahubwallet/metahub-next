79ea594795c82b69553850354dbb6a3e8ac30d45
16 <soup16-work@outlook.com>
2023-06-16 14:27:48 +0800

    feat: ImportProtocol

---
+++ src/entries/popup/wallet/ImportProtocol.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup></script>
+
+<template>
+    <div class="full-router">
+        <div class="full-inner">
+            <n-scrollbar class="full">
+                <page-header :title="$t('public.readAndAgreeProtocols')"></page-header>
+                <div class="cover-content _effect">
+                    <div class="import-protocol">
+                        {{ $t('public.viewUserProtocolsContent') }}
+                    </div>
+                </div>
+            </n-scrollbar>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.import-protocol {
+    background-color: white;
+    font-size: 15px;
+    margin: 15px;
+    text-indent: 2em;
+    color: #333333;
+}
+</style>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -2,6 +2,7 @@ import { RouteRecordRaw } from 'vue-router';
 
 import Wallet from '@/entries/popup/wallet/index.vue';
 import ImportKey from '@/entries/popup/wallet/ImportKey.vue';
+import ImportProtocol from '@/entries/popup/wallet/ImportProtocol.vue';
 
 export const routes: RouteRecordRaw[] = [
     {
@@ -16,6 +17,14 @@ export const routes: RouteRecordRaw[] = [
         path: '/wallet/import-key',
         name: 'import-key',
         component: ImportKey,
+        meta: {
+            index: 10,
+        },
+    },
+    {
+        path: '/wallet/import-protocol',
+        name: 'import-protocol',
+        component: ImportProtocol,
         meta: {
             index: 10,
         },
