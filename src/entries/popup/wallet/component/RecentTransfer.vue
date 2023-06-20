b87d391236d89ec922ec340f39f7408386af1da4
16 <soup16-work@outlook.com>
2023-06-20 16:26:26 +0800

    feat: RecentTransfer.vue

---
+++ src/entries/popup/wallet/component/RecentTransfer.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const wallet = store.wallet();
+const { timeFormat } = tool;
+</script>
+
+<template>
+    <n-scrollbar>
+        <div
+            v-for="(item, index) of wallet.recentTransations"
+            :key="index"
+            class="grid grid-cols-12 items-center border-gray-200 py-[10px] cursor-pointer"
+            :class="index ? 'border-t' : ''"
+            @click="$emit('select', item)"
+        >
+            <div class="col-span-7 ml-2">
+                {{ $t('wallet.receiverAccount') }}:
+                <span v-if="item.receiver.length == 42">
+                    {{ item.receiver.substring(0, 10) }}...{{ item.receiver.substring(36) }}
+                </span>
+                <span v-else>{{ item.receiver }}</span>
+            </div>
+
+            <div class="col-span-5">
+                {{ timeFormat(item.time) }}
+            </div>
+        </div>
+    </n-scrollbar>
+</template>
+
+<style lang="scss" scoped></style>
