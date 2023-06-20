a1bfafea57268a3532a6c57110bd7af0926dc95a
16 <soup16-work@outlook.com>
2023-06-20 16:26:43 +0800

    feat: SelectCoin.vue

---
+++ src/entries/popup/wallet/component/SelectCoin.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const wallet = store.wallet();
+</script>
+
+<template>
+    <n-scrollbar>
+        <div
+            v-for="(coin, index) of wallet.userTokens"
+            :key="index"
+            class="grid grid-cols-12 items-center border-gray-200 py-[10px] cursor-pointer"
+            :class="index ? 'border-t' : ''"
+            @click="$emit('changeToken', coin)"
+        >
+            <div class="col-span-2">
+                <img class="icon-img ml-[14px]" :src="coin.logo" />
+            </div>
+
+            <div class="col-span-8">
+                <div class="coin-symbol ml-[20px]">{{ coin.symbol }}</div>
+                <div class="coin-contract">{{ $t('wallet.contract') }}: {{ coin.contract }}</div>
+            </div>
+
+            <div class="col-span-2">{{ $t('wallet.balance') }}: {{ coin.amount }}</div>
+        </div>
+    </n-scrollbar>
+</template>
+
+<style lang="scss" scoped></style>
