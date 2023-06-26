f7b47a2b9f10d0e2d6e7de35a4ed3e81f01755bf
16 <soup16-work@outlook.com>
2023-06-26 10:33:21 +0800

    pack: transfer module

--- src/router/module/wallet.routes.ts
+++ src/router/module/wallet.routes.ts
@@ -3,10 +3,6 @@ import { RouteRecordRaw } from 'vue-router';
 import ImportKey from '@/entries/popup/wallet/ImportKey.vue';
 import ImportProtocol from '@/entries/popup/wallet/ImportProtocol.vue';
 import AddToken from '@/entries/popup/wallet/AddToken.vue';
-import TokenTraces from '@/entries/popup/wallet/TokenTraces.vue';
-import Transfer from '@/entries/popup/wallet/Transfer.vue';
-import Transation from '@/entries/popup/wallet/Transation.vue';
-import Receive from '@/entries/popup/wallet/Receive.vue';
 
 export default {
     path: '/wallet',
@@ -34,38 +30,6 @@ export default {
             component: AddToken,
             meta: {
                 index: 2,
-            },
-        },
-        {
-            path: 'token-traces/:token',
-            name: 'token-traces',
-            component: TokenTraces,
-            meta: {
-                index: 2,
-            },
-        },
-        {
-            path: 'transfer',
-            name: 'transfer',
-            component: Transfer,
-            meta: {
-                index: 3,
-            },
-        },
-        {
-            path: 'transation',
-            name: 'transation',
-            component: Transation,
-            meta: {
-                index: 3,
-            },
-        },
-        {
-            path: 'receive',
-            name: 'receive',
-            component: Receive,
-            meta: {
-                index: 3,
             },
         },
     ],
