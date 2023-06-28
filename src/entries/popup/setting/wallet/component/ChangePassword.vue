a1078b11420e23ba2d48848e9e3e586b555a6767
16 <soup16-work@outlook.com>
2023-06-28 11:05:06 +0800

    feat: ChangePassword.vue

---
+++ src/entries/popup/setting/wallet/component/ChangePassword.vue
@@ -0,0 +1,87 @@
+<script setup lang="ts">
+import { decrypt, encrypt, md5, password1, password2 } from '@/common/util/crypto';
+
+interface Props {
+    isShow: boolean;
+}
+const props = withDefaults(defineProps<Props>(), {});
+
+const passwordOld = ref('');
+const passwordNew = ref('');
+const passwordConfirm = ref('');
+
+const { t } = useI18n();
+const changePasswordHandle = async () => {
+    if (password2(passwordOld.value) != store.user().passwordHash)
+        return window.msg.warning(t('password.error'));
+
+    if (!passwordNew.value || !passwordConfirm.value)
+        return window.msg.warning(t('password.empty'));
+
+    if (passwordNew.value != passwordConfirm.value)
+        return window.msg.warning(t('public.passwordNoSame'));
+
+    // 密码存储
+    store.user().passwordHash = password2(passwordNew.value);
+    // 秘钥重新加密。。。
+    for (const wallet of store.wallet().wallets) {
+        for (const key of wallet.keys) {
+            let pk = decrypt(key.privateKey, md5(wallet.seed + password1(passwordOld.value)));
+            key.privateKey = encrypt(pk, md5(wallet.seed + password1(passwordNew.value)));
+        }
+    }
+    store.user().password = '';
+    store.setting().isLock = true;
+};
+</script>
+
+<template>
+    <n-modal :show="props.isShow" @mask-click="$emit('close')">
+        <n-card class="w-5/6" :title="$t('setting.changePassword')" :bordered="false">
+            <!-- old password -->
+            <div class="dialog-title">
+                {{ $t('setting.oldPassword') }}
+            </div>
+            <n-input
+                class="mb-3"
+                show-password
+                v-model:value="passwordOld"
+                :placeholder="$t('setting.oldPassword')"
+            ></n-input>
+
+            <!-- new password -->
+            <div class="dialog-title">
+                {{ $t('setting.newPassword1') }}
+            </div>
+            <n-input
+                class="mb-3"
+                show-password
+                v-model:value="passwordNew"
+                :placeholder="$t('setting.newPassword1')"
+            ></n-input>
+            <div class="dialog-title">
+                {{ $t('setting.newPassword2') }}
+            </div>
+
+            <!-- confirm password -->
+            <n-input
+                v-model:value="passwordConfirm"
+                show-password
+                :placeholder="$t('setting.newPassword2')"
+                clearable
+            ></n-input>
+
+            <!-- button -->
+            <div class="mt-5 flex justify-end">
+                <n-button @click="$emit('close')" class="mr-2">
+                    {{ $t('public.cancel') }}
+                </n-button>
+                <n-button type="primary" @click="changePasswordHandle">
+                    {{ $t('public.ok') }}
+                </n-button>
+            </div>
+        </n-card>
+    </n-modal>
+</template>
+
+<style lang="scss" scoped></style>
