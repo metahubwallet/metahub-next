fcdd75f1c237cf7ecb4f5305df4752d34250a104
mht <dev@metahub.cash>
2023-09-04 19:34:34 +0800

    remvoe log on build

--- config/vite/plugin/index.ts
+++ config/vite/plugin/index.ts
@@ -1,18 +1,20 @@
 import vue from '@vitejs/plugin-vue';
-import { Plugin } from 'vite';
+import { PluginOption } from 'vite';
 import nodePolyfills from 'vite-plugin-node-stdlib-browser';
+import removeConsole from "vite-plugin-remove-console";
 
 import { autoImport } from './autoImport';
 import { setupComponent } from './component';
 import { setupManifest } from './manifest';
 
 export const setupPlugins = () => {
-    let plugins: Plugin[] = [vue()];
-
+    const plugins: PluginOption[] = [];
+    plugins.push(vue());
     plugins.push(setupManifest());
     plugins.push(autoImport());
     plugins.push(setupComponent());
     plugins.push(nodePolyfills());
+    plugins.push(removeConsole());
 
     return plugins;
 };
