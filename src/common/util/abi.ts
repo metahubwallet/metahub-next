5acdd9e2ba2003df79f43ed8deeab5b929767198
mht <dev@metahub.cash>
2024-03-01 16:03:34 +0800

    fix abi cache

--- src/common/util/abi.ts
+++ src/common/util/abi.ts
@@ -32,16 +32,15 @@ export const getContractAbi = async (api: Api, chainId: string, contract: string
         const rawAbi = api.jsonToRawAbi(abi);
         return { abi, raw: rawAbi };
     }
-    
     const cachedABI = await getCachedABI(chainId, contract);
     const nowTime = new Date().getTime();
     
-    if (cachedABI && cachedABI.expire && cachedABI.expire > nowTime) {
+    if (cachedABI && cachedABI.abi && Array.isArray(cachedABI.raw) && cachedABI.expire && cachedABI.expire > nowTime) {
         const codeUpdateTime = new Date(
             ((await api.rpc.get_account(contract)) as any).last_code_update + 'Z'
         ).getTime();
         if (cachedABI.updated > codeUpdateTime) {
-            return { abi: cachedABI.abi, raw: cachedABI.raw };
+            return { abi: cachedABI.abi, raw: Uint8Array.from(cachedABI.raw) };
         }
     }
 
@@ -49,20 +48,17 @@ export const getContractAbi = async (api: Api, chainId: string, contract: string
     const rawAbi = await api.rpc.get_raw_abi(contract);
     const raw = base64ToBinary(rawAbi.abi);
     
-    abi = api.rawAbiToJson(raw);
-
-    
+    abi = api.rawAbiToJson(raw); 
 
     const savableAbi: CacheABI = {
         chainId: chainId,
         contract,
         abi,
-        raw,
+        raw: Array.from(raw),
         // hash: rawAbi.abi_hash,
         updated: nowTime,
         expire: nowTime + 86400000 * 7,
     };
     await setCacheABI(savableAbi);
-
     return { abi, raw };
 }
